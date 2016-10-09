7fea8434b4a7f5c83832480e3400f5d862f6bbef
man chon <mckuok@hawaii.edu>
2016-10-08 22:07:24 -1000

    First commit

---
+++ test/trellogitintegration/json/SampleJsonData.java
@@ -0,0 +1,90 @@
+package trellogitintegration.json;
+
+public class SampleJsonData {
+
+	private int userId;
+	private int id;
+	private String title;
+	private String body;
+
+	public SampleJsonData() {}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "JsonData [userId=" + userId + ", id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + userId;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SampleJsonData other = (SampleJsonData) obj;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+	
+
+}
---
+++ test/trellogitintegration/utils/RestApiConnectorTest.java
@@ -0,0 +1,56 @@
+package trellogitintegration.utils;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.junit.Test;
+
+import trellogitintegration.json.SampleJsonData;
+
+public class RestApiConnectorTest {
+	
+	private static final String root = "https://jsonplaceholder.typicode.com";
+	
+	@Test
+	public void getTest() throws IOException {
+		SampleJsonData expectedObject = new SampleJsonData();
+		expectedObject.setId(1);
+		expectedObject.setUserId(1);
+		expectedObject.setBody("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
+		expectedObject.setTitle("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+		
+		SampleJsonData returnedObject = RestApiConnector.get(root + "/posts/1", SampleJsonData.class);
+		assertEquals(expectedObject, returnedObject);	
+	} 
+	
+	@Test
+	public void postTest() throws IOException {
+		SampleJsonData jsonObject = new SampleJsonData();
+		jsonObject.setUserId(1);
+		jsonObject.setBody("body");
+		jsonObject.setTitle("title");
+		
+		int statusCode = RestApiConnector.post(root + "/posts", jsonObject);
+		assertEquals(HttpURLConnection.HTTP_CREATED, statusCode);
+	}
+	
+	@Test
+	public void putTest() throws IOException {
+		SampleJsonData jsonObject = new SampleJsonData();
+		jsonObject.setUserId(1);
+		jsonObject.setBody("body");
+		jsonObject.setTitle("title");
+		
+		int statusCode = RestApiConnector.put(root + "/posts/1", jsonObject);
+		assertEquals(HttpURLConnection.HTTP_OK, statusCode);
+	}
+	
+	@Test
+	public void deleteTest() throws IOException {
+		int statusCode = RestApiConnector.delete(root + "/posts/1");
+		assertEquals(HttpURLConnection.HTTP_OK, statusCode);
+	}
+
+}
