7c130dd0ab27aa08a14e0435271ead915e43e610
man chon <mckuok@hawaii.edu>
2016-11-06 10:45:19 -1000

    Add javadoc

--- src/trellogitintegration/eclipse/utils/UIUtils.java
+++ src/trellogitintegration/eclipse/utils/UIUtils.java
@@ -17,36 +17,59 @@ public class UIUtils {
 
   /**
    * Add a label to the parent
-   * @param parent parent of the label
-   * @param text default lable message
+   * 
+   * @param parent
+   *          parent of the label
+   * @param text
+   *          default lable message
    */
   public static void addLabel(Composite parent, String text) {
     Label label = new Label(parent, SWT.NONE);
     label.setText(text);
   }
-  
+
   /**
+   * Add an input text box to the UI
    * 
    * @param parent
+   *          parent of the text box
    * @param gridData
+   *          gridData used to describe the layout of the textbox
    * @param defaultText
+   *          default text for the textbox
    * @return
    */
-  public static Text addInputTextBox(Composite parent, GridData gridData, String defaultText) {
+  public static Text addInputTextBox(Composite parent, GridData gridData,
+      String defaultText) {
     Text textbox = new Text(parent, SWT.SEARCH | SWT.SINGLE | SWT.BORDER);
     textbox.setLayoutData(gridData);
     textbox.setText(defaultText);
     return textbox;
-  } 
-  
+  }
+
+  /**
+   * Add an horizontal separator to the UI
+   * 
+   * @param parent
+   *          parent of the separator
+   */
   public static void addSeparator(Composite parent) {
     Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
     GridData gridData = new GridData();
     gridData.horizontalAlignment = GridData.FILL;
     gridData.grabExcessHorizontalSpace = true;
     separator.setLayoutData(gridData);
-  }  
-  
+  }
+
+  /**
+   * Uniform all buttons' sizes. The buttons must exist in the the UI (already
+   * populated)
+   * 
+   * @param parent
+   *          parent of the buttons
+   * @param buttons
+   *          buttons to be resized
+   */
   public static void uniformButtonSize(Composite parent, Button... buttons) {
     parent.layout();
     int maxHeight = 0;
@@ -60,30 +83,47 @@ public class UIUtils {
         maxHeight = size.y;
       }
     }
-    
+
     GridData gridData = new GridData();
     gridData.widthHint = maxWidth;
     gridData.heightHint = maxHeight;
-    
+
     for (int i = 0; i < buttons.length; i++) {
       buttons[i].setLayoutData(gridData);
     }
   }
-  
+
+  /**
+   * Center a child on the parent
+   * 
+   * @param parent
+   *          parent
+   * @param child
+   *          child
+   */
   public static void centerShell(Composite parent, Composite child) {
     Monitor primary = parent.getDisplay().getPrimaryMonitor();
     Rectangle bounds = primary.getBounds();
     Rectangle rect = child.getBounds();
-    
+
     int x = bounds.x + (bounds.width - rect.width) / 2;
     int y = bounds.y + (bounds.height - rect.height) / 2;
-    
+
     child.setLocation(x, y);
   }
-  
+
+  /**
+   * Creates a default container with grid layout
+   * 
+   * @param parent
+   *          parent of the container
+   * @param numOfColumns
+   *          number of columns in the grid
+   * @return a created container
+   */
   public static Composite createContainer(Composite parent, int numOfColumns) {
-  ValidationUtils.checkNull(parent);
-    
+    ValidationUtils.checkNull(parent);
+
     Composite container = new Composite(parent, SWT.NONE);
     GridLayout layout = new GridLayout();
     layout.numColumns = numOfColumns;
