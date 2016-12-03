5de58e02a96e83d3dc3f4df0f9f3cd0aee73a9a0
ungarz <leonq@hawaii.edu>
2016-12-02 18:27:04 -1000

    Test Case added

--- test/pomodoro/exec/PomodoroExecutorTest.java
+++ test/pomodoro/exec/PomodoroExecutorTest.java
@@ -3,10 +3,14 @@ package pomodoro.exec;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import Pomodoro.PomodoroTimer;
 import Pomodoro.exec.PomodoroExecutor;
+import trellogitintegration.persist.config.ProjectConfig.PomodoroConfig;
 
 public class PomodoroExecutorTest {
 
+  private PomodoroConfig config;
+  private PomodoroTimer time;
   private PomodoroExecutor exec;
   
 	@Test
@@ -14,8 +18,14 @@ public class PomodoroExecutorTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testCount(){
-	  
+  @Test
+	public void testCounts(){
+	  time = new PomodoroTimer(config);
+	  time.refreshPomodoroTimer();
+	  assertEquals(0, time.getPomodoroTime());
+	  assertEquals(0, time.getPomodoros());
+	  assertEquals(5, time.getBreakTime());
+	  assertEquals(10, time.getLongBreakTime());
 	}
+  
 }
